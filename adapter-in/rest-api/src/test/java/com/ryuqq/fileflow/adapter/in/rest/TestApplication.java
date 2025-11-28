a3b978dcd422801163f73101aedb2a44d59900c7
sangwon-ryu <fbtkdals2@naver.com>
2025-11-28 11:31:09 +0900

    fix: REST API 모듈에 TestApplication 추가
    @WebMvcTest를 사용하는 테스트에서 @SpringBootConfiguration을
    찾을 수 없는 문제 수정

---
+++ adapter-in/rest-api/src/test/java/com/ryuqq/fileflow/adapter/in/rest/TestApplication.java
@@ -0,0 +1,11 @@
+package com.ryuqq.fileflow.adapter.in.rest;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * REST API 모듈 테스트용 Spring Boot Application.
+ *
+ * <p>@WebMvcTest를 사용하는 테스트에서 @SpringBootConfiguration을 찾을 수 있도록 제공합니다.
+ */
+@SpringBootApplication
+public class TestApplication {}
