8beab1f242184e803d0e152b79ea30cfcaa477f8
sangwon-ryu <fbtkdals2@naver.com>
2025-12-16 01:14:10 +0900

    style: Spotless 포맷팅 적용

--- adapter-in/rest-api/src/main/java/com/ryuqq/fileflow/adapter/in/rest/auth/config/CorsProperties.java
+++ adapter-in/rest-api/src/main/java/com/ryuqq/fileflow/adapter/in/rest/auth/config/CorsProperties.java
@@ -2,7 +2,6 @@ package com.ryuqq.fileflow.adapter.in.rest.auth.config;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
